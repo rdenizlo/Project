fb121c24d9004a9c67175048c7ebf875bcbd88e1
TLChilton <thomaschilton93@gmail.com>
2019-11-27 20:08:33 -0700

    Initial formatter changes

--- src/logic/Formatter.java
+++ src/logic/Formatter.java
@@ -3,9 +3,29 @@ package logic;
 public class Formatter {
 
     private String input;
+    private int lineSize;
+    private boolean indent, wrap, isSingleSpaced, isSingleColumn;
+    private enum Justified {
+        Left, Right, Center, Equal;
+    }
+    private Justified justType;
+
+    public Formatter(String i) {
+        input = i;
+        lineSize = 80;
+        wrap = false;
+        isSingleSpaced = true;
+        isSingleColumn = true;
+        justType = Justified.Left;
+    }
 
     public Formatter() {
-
+        input = "No Input Exists";
+        lineSize = 80;
+        wrap = false;
+        isSingleSpaced = true;
+        isSingleColumn = true;
+        justType = Justified.Left;
     }
 
     // TODO - Example method, remove after
@@ -13,7 +33,9 @@ public class Formatter {
         this.input = input;
     }
 
+
     public String getOutput() {
+
         return input;
     }
 
