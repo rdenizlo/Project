990e631b79f238abd7f69c5f873801ef9f22accd
TLChilton <thomaschilton93@gmail.com>
2019-12-01 16:55:24 -0700

    Formatter initial setup

--- src/logic/Formatter.java
+++ src/logic/Formatter.java
@@ -21,9 +21,76 @@ public class Formatter {
     }
 
 
-    public String getOutput() {
-        
+    public String getOutput(String preParsed) {
+        input = preParsed;
+        String linecheck;
+        String output = "";
+        int i = 0;
+        int j = 0;
+        String[] splitDoc = new String[50];
+        while(i < input.length())
+        {
+            splitDoc[j] = "";
+            while(input.charAt(i) != '\n' && i < input.length())
+            {
+                splitDoc[j] = splitDoc[j] + input.charAt(i);
+                ++i;
+            }
+            ++j;
+        }  
+
+        for(i = 0; i < splitDoc.length; ++i)
+        {
+            if(splitDoc[i].length() <= 3)
+            {
+                if(splitDoc[i].charAt(0) = '-')
+                {
+                    linecheck = splitDoc[i];
+                    Command check = new Command(linecheck);
+                }
+                else
+                {
+                    output = output + splitDoc[i];
+                }
+            }
+            else
+            {
+                output = output + splitDoc[i];
+            }
+        }
         return output;
     }
 
+    private commandHandler(Command foundCommand)
+    {
+        Command check = foundCommand;
+        switch (check) {
+            case 'CommandType.CHARACTER':
+                
+                break;
+            case 'CommandType.RIGHT':
+                break;
+            case 'CommandType.LEFT':
+                break;
+            case 'CommandType.CENTER':
+                break;
+            case 'CommandType.EQUAL':
+                break;
+            case 'CommandType.WRAP':
+                break;
+            case 'CommandType.SINGLE':
+                break;
+            case 'CommandType.DOUBLE':
+                break;
+            case 'CommandType.TITLE':
+                break;
+            case 'CommandType.INDENT':
+                break;
+            case 'CommandType.BLANK':
+                break;
+            case 'CommandType.COLUMN':
+                break;
+            default:
+    }
 }
+
