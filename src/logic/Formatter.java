b110fb7ebb213bbe2eb7157e18659fa7118fe345
TLChilton <thomaschilton93@gmail.com>
2019-12-01 18:00:22 -0700

    Formatter initial setup

--- src/logic/Formatter.java
+++ src/logic/Formatter.java
@@ -21,9 +21,93 @@ public class Formatter {
     }
 
 
-    public String getOutput() {
-        
+    public String getOutput(String preParsed) {
+        input = preParsed;
+        String linecheck;
+        String output = "";
+        int i = 0;
+        int j = 0;
+        String[] splitDoc = new String[50];
+        while(i < input.length())
+        {
+            splitDoc[j] = "";
+            while(input.charAt(i) != '\n' && i < input.length())
+            {
+                splitDoc[j] = splitDoc[j] + input.charAt(i);
+                ++i;
+            }
+            ++j;
+        }  
+
+        for(i = 0; i < splitDoc.length; ++i)
+        {
+            if(splitDoc[i].length() <= 5)
+            {
+                if(splitDoc[i].charAt(0) == '-')
+                {
+                    linecheck = splitDoc[i];
+                    Command check = new Command(linecheck);
+                    output = commandHandler(check, output);
+                }
+                else
+                {
+                    output = output + splitDoc[i];
+                }
+            }
+            else
+            {
+                output = output + splitDoc[i];
+            }
+        }
         return output;
     }
 
+    private String commandHandler(Command foundCommand, String input)
+    {
+        String output = input
+        Command check = foundCommand;
+        switch (check) {
+            case 'CommandType.CHARACTER': // needs to be implemented
+                
+                break;
+            case 'CommandType.RIGHT':
+                justType = Justified.Right;
+                break;
+            case 'CommandType.LEFT':
+                justType = Justified.Left;
+                break;
+            case 'CommandType.CENTER':
+                justType = Justified.Center;
+                break;
+            case 'CommandType.EQUAL':
+                justType = Justified.Equal;
+                break;
+            case 'CommandType.WRAP_MINUS':
+                wrap = false;
+                break;
+            case 'CommandType.WRAP_PLUS':
+                wrap = true;
+                break;
+            case 'CommandType.SINGLE':
+                isSingleSpaced = true;
+                break;
+            case 'CommandType.DOUBLE':
+                isSingleSpaced = false;
+                break;
+            case 'CommandType.TITLE':   // needs to be implemented
+                break;
+            case 'CommandType.INDENT': // needs to be implemented
+                break;
+            case 'CommandType.BLANK': // needs to be implemented
+                
+                break;
+            case 'CommandType.SINGLE_COLUMN':
+                isSingleColumn = true;
+                break;
+            case 'CommandType.DOUBLE_COLUMN':
+                isSingleColumn = false;
+                break;
+            default:
+    }
 }
+
