3a506efd0fb72d99b8e7ca85bf5884a7b0489950
TLChilton <thomaschilton93@gmail.com>
2019-12-02 20:58:03 -0700

    Ramon's changes

--- test/test/FormatterTest.java
+++ test/test/FormatterTest.java
@@ -1,8 +1,9 @@
 package test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 import logic.FileReader;
 import logic.Formatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class FormatterTest {
 
@@ -22,7 +23,52 @@ class FormatterTest {
         String file = "Case1";
         String input = getInput(file);
         String output = getOutput(file);
-        Assertions.assertEquals(formatter.getOutput(input), output);
+        Assertions.assertEquals(output, formatter.getOutput(input));
+    }
+
+    @Test
+    public void testCase2(){
+        Formatter formatter = new Formatter();
+        String file = "Case2";
+        String input = getInput(file);
+        String output = getOutput(file);
+        Assertions.assertEquals(output, formatter.getOutput(input));
+    }
+
+    @Test
+    public void testCase3(){
+        Formatter formatter = new Formatter();
+        String file = "Case3";
+        String input = getInput(file);
+        String output = getOutput(file);
+        Assertions.assertEquals(output, formatter.getOutput(input));
+    }
+
+    @Test
+    public void testCase4(){
+        Formatter formatter = new Formatter();
+        String file = "Case4";
+        String input = getInput(file);
+        String output = getOutput(file);
+        Assertions.assertEquals(output, formatter.getOutput(input));
+    }
+
+    @Test
+    public void testCase5(){
+        Formatter formatter = new Formatter();
+        String file = "Case5";
+        String input = getInput(file);
+        String output = getOutput(file);
+        Assertions.assertEquals(output, formatter.getOutput(input));
+    }
+
+    @Test
+    public void testCase6(){
+        Formatter formatter = new Formatter();
+        String file = "Case6";
+        String input = getInput(file);
+        String output = getOutput(file);
+        Assertions.assertEquals(output, formatter.getOutput(input));
     }
 
 }
